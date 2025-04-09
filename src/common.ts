acd4b05d7f2d780b42c2e583941d230db639c0e9
Hannes <schroffl@users.noreply.github.com>
2025-04-09 22:23:26 +0200

    Remove unused randomColor function

--- src/common.ts
+++ src/common.ts
@@ -46,19 +46,6 @@ export function randomID(length: number = 8): string {
     return out;
 }
 
-export function randomColor(): string {
-    let out = '#';
-
-    for (let i = 0; i < 3; i++) {
-        const rand = Math.random();
-        const byte = Math.floor(rand * 0xff);
-
-        out += byte.toString(16).padStart(2, '0');
-    }
-
-    return out;
-}
-
 export type MathFunction = {
     readonly fn: (x: number) => number;
     num_samples?: number;
